openwrt/ugps

commit b88037b6bf6a6c812183566d96887cb0b201491b

check timegm return code
Signed-off-by: John Crispin <john@phrozen.org>

nmea.c
@@ -97,12 +97,14 @@ nmea_rmc_cb(void)
 		DEBUG(3, "date: %s UTC\n", tmp);
 
 		if (adjust_clock) {
-			struct timeval tv = { timegm(&tm), 0 };
+			time_t sec = timegm(&tm);
+			struct timeval tv = { 0 };
 			struct timeval cur;
 
 			gettimeofday(&cur, NULL);
 
-			if (abs(cur.tv_sec - tv.tv_sec) > MAX_TIME_OFFSET) {
+			if ((sec < 0) || (abs(cur.tv_sec - tv.tv_sec) > MAX_TIME_OFFSET)) {
+				tv.tv_sec = sec;
 				if (++nmea_bad_time > MAX_BAD_TIME) {
 					LOG("system time differs from GPS time by more than %d seconds. Using %s UTC as the new time\n", MAX_TIME_OFFSET, tmp);
 					/* only set datetime if specified by command line argument! */

commit db40e3a22599641be68c88ece287d3fbbacd8a45

nmea: fix time comparision
Code introduced by commit b88037b ("check timegm return code") compares
GPS-time with tv_sec of freshly zero-initialized struct timeval tv.
Compare with current time instead.

Fixes: b88037b ("check timegm return code")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

nmea.c
@@ -99,12 +99,12 @@ nmea_rmc_cb(void)
 
 		if (adjust_clock) {
 			time_t sec = timegm(&tm);
-			struct timeval tv = { 0 };
 			struct timeval cur;
 
 			gettimeofday(&cur, NULL);
 
-			if ((sec < 0) || (abs(cur.tv_sec - tv.tv_sec) > MAX_TIME_OFFSET)) {
+			if ((sec < 0) || (abs(cur.tv_sec - sec) > MAX_TIME_OFFSET)) {
+				struct timeval tv = { 0 };
 				tv.tv_sec = sec;
 				if (++nmea_bad_time > MAX_BAD_TIME) {
 					LOG("system time differs from GPS time by more than %d seconds. Using %s UTC as the new time\n", MAX_TIME_OFFSET, tmp);