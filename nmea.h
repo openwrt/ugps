f203ff6921bce67077fad2438c1c88046798033e
Zbyněk Kocur <zbynek.kocur@fel.cvut.cz>
2023-09-21 14:24:32 +0200

    Update nmea.h
    typo fix
    
    Signed-off-by: Zbyněk Kocur <zbynek.kocur@fel.cvut.cz>

--- nmea.h
+++ nmea.h
@@ -23,7 +23,7 @@
 
 #include <libubox/ustream.h>
 
-extern char longitude[33], latitude[33], course[17], speed[17], elevation[17], satellites[3], hdop[5];;
+extern char longitude[33], latitude[33], course[17], speed[17], elevation[17], satellites[3], hdop[5];
 extern int nmea_open(char *dev, struct ustream_fd *s, speed_t speed);
 extern void gps_timestamp(void);
 extern unsigned int adjust_clock;
